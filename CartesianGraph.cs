bd21b6668285d652defc72fccdbbcd85e75fe92a
Kevin Wang <wangkev@umich.edu>
2018-08-07 20:41:09 -0400

    added X edge functionality, cleaned up code

--- CartesianGraph.cs
+++ CartesianGraph.cs
@@ -42,61 +42,40 @@ namespace ConsoleApplication1 {
 
                 bool edgeH = (c % 2 != 0);
                 bool edgeV = (r % 2 != 0);
-                char kz = mapData[i];
-
-                Vertex point1, point2;
 
                 if (mapData[i] == '-' || mapData[i] == '|' || mapData[i] == '\\') {
-                    point1 = findVertex(c / 2, r / 2);
-                    point2 = findVertex((c + (edgeH ? 1 : 0)) / 2, (r + (edgeV ? 1 : 0)) / 2);
-                    point1.edges.Add(point2);
-                    point2.edges.Add(point1);
+                    addEdge(edgeH, edgeV, c, r, false);
                 }
 
                 // If NE/SW edge
                 if (edgeH && edgeV) {
                     if (mapData[i] == '/') {
-                        point1 = findVertex((c - 1) / 2, (r + 1) / 2);
-                        point2 = findVertex((c + 1) / 2, (r - 1) / 2);
-                        point1.edges.Add(point2);
-                        point2.edges.Add(point1);
+                        addEdge(edgeH, edgeV, c, r, true);
                     } else if(mapData[i] == 'X') {
-                        point1 = findVertex((c - 1) / 2, (r + 1) / 2);
-                        point2 = findVertex((c + 1) / 2, (r - 1) / 2);
-                        point1.edges.Add(point2);
-                        point2.edges.Add(point1);
-                        
-                        point1 = findVertex(c / 2, r / 2);
-                        point2 = findVertex((c + (edgeH ? 1 : 0)) / 2, (r + (edgeV ? 1 : 0)) / 2);
-                        point1.edges.Add(point2);
-                        point2.edges.Add(point1);
+                        addEdge(edgeH, edgeV, c, r, true);
+                        addEdge(edgeH, edgeV, c, r, false);
                     }
-                }
-            }
-        }
-
-        public CartesianGraph(string inFile, bool k) {
-            // example said: @"C:\Users\Public\TestFolder\WriteText.txt"
-            string text = System.IO.File.ReadAllText(inFile);
-            string mapData = getBetween(text, "<MAP>", "</MAP>");
-            string[] words = mapData.Split(null);
-            // for all vertices
-            for(int i = 0; i < words.Length; ++i) {
-                if (words[i].Length > 0 && words[i][0] == '(') {
-                    Vertex temp = parseVertex(words[i]);
-                    vertices.Add(temp);
-                }
-            }
-            // for all edges
-            for (int i = 0; i < words.Length; ++i) {
-                if (words[i].Length > 0 && char.IsNumber(words[i][0])) {
-                    parseEdge(words[i]);
                 }
             }
         }
 
         private List<Vertex> vertices = new List<Vertex>();
 
+        // Adds edges between two points
+        private void addEdge(bool edgeH, bool edgeV, int c, int r, bool ne) {
+            Vertex point1 = new Vertex();
+            Vertex point2 = new Vertex();
+            point1 = ne ? findVertex((c - 1) / 2, (r + 1) / 2) :
+                findVertex(c / 2, r / 2);
+            point2 = ne ? findVertex((c + 1) / 2, (r - 1) / 2) :
+                findVertex((c + (edgeH ? 1 : 0)) / 2, (r + (edgeV ? 1 : 0)) / 2);
+            point1.edges.Add(point2);
+            point2.edges.Add(point1);
+        }
+
+        // Manages the row and column of the current character
+        // r = -1 is "standby" for start of grid not read yet
+        // c = -1 because \n counts as column 0 for each line
         private void iterateGrid(char myChar, ref int r, ref int c) {
             if (myChar == '\n') {
                 if (r > -1) ++r;
@@ -116,52 +95,6 @@ namespace ConsoleApplication1 {
             } else {
                 return "";
             }
-        }
-
-        // Returns a vertex with x and y values specified by format (XXYY)
-        private static Vertex parseVertex(string vertex) {
-            Vertex retVal = new Vertex();
-            retVal.x = Int32.Parse(vertex.Substring(1, 2));
-            retVal.y = Int32.Parse(vertex.Substring(3, 2));
-            return retVal;
-        }
-
-        // Interprets edge notation 
-        private void parseEdge(string edge) {
-            int x = Int32.Parse(edge.Substring(0, 2));
-            int y = Int32.Parse(edge.Substring(2, 2));
-            string direction = edge.Substring(4, 2);
-            int xOffset = 0;
-            int yOffset = 0;
-            Vertex vertex = findVertex(x, y);
-            if (vertex.x == -1) {
-                Console.WriteLine("ERROR: Couldn't parse edge.");
-                return;
-            }
-
-            switch (direction[0]) {
-                case 'N': yOffset = -1;
-                    break;
-                case 'S': yOffset = 1;
-                    break;
-            }
-            switch (direction[1]) {
-                case 'E': xOffset = 1;
-                    break;
-                case 'W': xOffset = -1;
-                    break;
-            }
-            Vertex toVertex = findVertex(vertex.x + xOffset, vertex.y + yOffset);
-            addEdge(vertex, toVertex);
-            addEdge(toVertex, vertex);
-        }
-
-        // Adds an edge to a vertex if its values are between [0, 99]
-        private void addEdge(Vertex vertex, Vertex edge) {
-            if (edge.x >= 0 && edge.x < 100 && edge.y >= 0 && edge.y < 100)
-                vertex.edges.Add(edge);
-            else
-                Console.WriteLine("ERROR: Edge out of bounds.");
         }
 
         // Returns a reference to a vertex at the specified (x, y)
